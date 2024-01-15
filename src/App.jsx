4453167a54ad9e704616e0775bae66ab18936104
lucenstuff <lucentiniagustin@hotmail.com>
2024-01-15 14:08:41 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
