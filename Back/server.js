d9ef84844eb0e6c74f1663604d1b3ee00d14c1cc
lucenstuff <lucentiniagustin@hotmail.com>
2024-02-21 21:50:08 -0300

    added fucntions for cart management

--- Back/server.js
+++ Back/server.js
@@ -310,6 +310,16 @@ app.get("/api/products/:productId", async (req, res) => {
   }
 });
 
+// app.get("/api/getcart/:userId", cors(), async (req, res) => {
+//   try {
+//     const userId = req.params.userId;
+//     const activeCart = await ShoppingCart.findOne({
+//       where: {
+//         UserID: userId,
+//         CartStatus: "active",
+//       },
+//     });)
+
 app.post("/api/addtocart/:id", cors(), async (req, res) => {
   try {
     const productId = req.params.id;
