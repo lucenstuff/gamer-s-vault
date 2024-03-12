e1c7ee4230627f180260787c2cf454ab0f837a60
lucenstuff <lucentiniagustin@hotmail.com>
2024-03-12 16:43:29 -0300

    fixes in homepage

--- src/components/FeaturedGame.jsx
+++ src/components/FeaturedGame.jsx
@@ -1,23 +1,23 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+
 export default class FeaturedGame extends Component {
   render() {
     const { gameImg, gameId, gameName } = this.props;
 
     return (
       <div className="p-2">
-        <div className="group relative overflow-hidden ease-in-out duration-200 rounded-md shadow-neutral-500 shadow-md">
-          <img
-            className="rounded-md transform group-hover:scale-105 duration-200"
-            src={gameImg}
-            alt={gameName}
-          />
-
-          <Link to={`/games/${gameId}`}>
-            <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 duration-200  cursor-pointer"></div>
-          </Link>
-        </div>
+        <Link to={`/games/${gameId}`}>
+          <div className="group relative overflow-hidden ease-in-out duration-200 rounded-md shadow-neutral-500 shadow-md ">
+            <img
+              src={gameImg}
+              alt={gameName}
+              className="w-full h-1/2 md:h-full md:w-full object-cover transition-transform  rounded-md transform group-hover:scale-105 duration-200 "
+              style={{ transformOrigin: "center center" }}
+            />
+          </div>
+        </Link>
       </div>
     );
   }
--- src/components/TrendingGames.jsx
+++ src/components/TrendingGames.jsx
@@ -1,42 +1,54 @@
+import React, { Component } from "react";
 import FeaturedGame from "./FeaturedGame";
-import { Component } from "react";
+import { getTrendingProducts } from "../services/apiConnection";
 
 export default class TrendingGames extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      trendingProducts: [],
+    };
+  }
+
+  componentDidMount() {
+    getTrendingProducts()
+      .then((products) => {
+        this.setState({ trendingProducts: products, isLoading: false });
+      })
+      .catch((error) => {
+        console.error("Failed to fetch products:", error);
+        this.setState({ isLoading: false });
+      });
+  }
+
   render() {
+    const { trendingProducts, isLoading } = this.state;
+
     return (
       <section className="lg:px-20 px-2">
         <h2 className="text-xl font-bold text-center py-6">
           JUEGOS DESTACADOS
         </h2>
-        <div className="flex flex-col md:flex-row ">
+        <div className="flex flex-col md:flex-row">
           <div className="w-full md:w-1/2">
-            <FeaturedGame
-              gameImg="https://res.cloudinary.com/dcbmvyyes/image/upload/v1697317279/games_db_images_compressed/ddakgrie2xsdoebwe7wa.jpg"
-              gameId="facb5988-b5a2-11ee-98dd-daaf184304ce"
-              gameName="Baldur's Gate 3"
-            />
+            {trendingProducts.length > 0 && (
+              <FeaturedGame
+                gameImg={trendingProducts[0].ImageURL}
+                gameId={trendingProducts[0].ProductID}
+                gameName={trendingProducts[0].ProductName}
+              />
+            )}
           </div>
-          <div className="w-full md:w-1/2 grid grid-cols-2">
-            <FeaturedGame
-              gameImg="https://res.cloudinary.com/dcbmvyyes/image/upload/v1697317280/games_db_images_compressed/t3ofshvjkbiimbekayfu.jpg"
-              gameId="facb610b-b5a2-11ee-98dd-daaf184304ce"
-              gameName="Disco Elysum"
-            />
-            <FeaturedGame
-              gameImg="https://res.cloudinary.com/dcbmvyyes/image/upload/v1697317278/gallery/grwp9e6sgt3goezbk4fn.jpg"
-              gameId="facb7533-b5a2-11ee-98dd-daaf184304ce"
-              gameName="Resident Evil 4 Remake"
-            />
-            <FeaturedGame
-              gameImg="https://res.cloudinary.com/dcbmvyyes/image/upload/v1697317281/games_db_images_compressed/fl0nmrtz9nonew8wjw0q.jpg"
-              gameId="facb6ab3-b5a2-11ee-98dd-daaf184304ce"
-              gameName="Gris"
-            />
-            <FeaturedGame
-              gameImg="https://res.cloudinary.com/dcbmvyyes/image/upload/v1697317280/games_db_images_compressed/uz2xik34di6cjjkbosgv.jpg"
-              gameId="facb61d5-b5a2-11ee-98dd-daaf184304ce"
-              gameName="Doom Eternal"
-            />
+          <div className="w-full md:w-1/2 grid grid-cols-1 md:grid-cols-2">
+            {trendingProducts.slice(1, 5).map((product) => (
+              <FeaturedGame
+                key={product.ProductID}
+                gameImg={product.ImageURL}
+                gameId={product.ProductID}
+                gameName={product.ProductName}
+              />
+            ))}
           </div>
         </div>
       </section>
