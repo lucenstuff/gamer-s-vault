3850d3d0cfcabf5e0b6659a7838367cbb6d81bf2
lucenstuff <lucentiniagustin@hotmail.com>
2024-03-13 12:17:48 -0300

    changes in back for vercel

--- Back/database/models/database/sequelize.js
+++ Back/database/models/database/sequelize.js
@@ -1,22 +1,31 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize({
+const options = {
   dialect: "mysql",
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  dialect: "mysql",
+  dialectModule: "mysql2",
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
     },
   },
   logging: false,
-});
+};
+
+if (options.dialect === "mysql") {
+  options.dialectModule = mysql2;
+}
+
+const sequelize = new Sequelize(options);
 
 sequelize
   .authenticate()
